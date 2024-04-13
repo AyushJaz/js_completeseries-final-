085048f0b12926f53242b3c16ec5e644c174e778
AyushJaz <ajayush2004@gmail.com>
2024-04-13 19:24:46 +0530

    container config done

--- loop.js
+++ loop.js
@@ -16,12 +16,12 @@
 // }
 
 
-// +++++++++++ for of loop++++++++++
+// +++++++++++ for of loop - works on both key and value 
 
 let arr=[1,2,3,4,5,6,7]
-for (const number of arr) {
-    console.log(number);
-}
+// for (const number of arr) {
+//     console.log(number);
+// }
 
 //maps
 
@@ -29,6 +29,23 @@ const map =new Map();
 map.set('IN','INDIA');
 map.set('USA','UNITED STATE OF AMERICA')
 map.set('FR',"Frrance");
-for (const [key,value] of map) {
-    console.log(`${key} - ${value}`);
-}
+// for (const [key,value] of map) {
+//     console.log(`${key} - ${value}`);
+// }
+
+
+
+//for in loops - only works on keys ,does not work on map
+
+
+let newOj=[
+    "Ayush",
+    "Aryan",
+    "Abhivyakt",
+    "Dhananjy",
+    "Krish",
+    "Yuvraj",
+]
+for (const key in newOj) {
+    console.log(key);
+} 
