e1d588afe5c312b23f03322b0f59163d8511d6c5
AyushJaz <ajayush2004@gmail.com>
2024-04-13 18:58:44 +0530

    container config done

---
+++ loop.js
@@ -0,0 +1,16 @@
+// for
+for(let i=0;i<=10;i++){
+    const c=i;
+    if(c===5)
+    console.log("5 is the best number");    
+console.log(`${c}`);
+}
+
+for(let i=1;i<=10;i++)
+{
+    let row='';
+    for (let j = 1; j <=i; j++) {
+        row+=`${j} `
+    }
+    console.log(row);
+}
