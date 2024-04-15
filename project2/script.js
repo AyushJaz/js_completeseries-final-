d9ab2d1fe488a8742ec30b6a74cc9acbadc1414c
AyushJaz <ajayush2004@gmail.com>
2024-04-15 12:31:11 +0530

    MInor update

--- project2/script.js
+++ project2/script.js
@@ -1,9 +1,17 @@
 
- let calculate=document.getElementById('calculate');
- calculate.addEventListener('click',()=>{
-    let height=parseInt(document.getElementById('height').value);
-let weight =parseInt(document.getElementById('weight').value);
-    let bmi=((weight)/((height/100)**2));
-    let result=document.querySelector('#result');
-    result.textContent=`Result : ${bmi}`
- })
+let calculate = document.getElementById('calculate');
+let result = document.querySelector('#result');
+ calculate.addEventListener('click', () => {
+    let height = parseInt(document.getElementById('height').value);
+    let weight = parseInt(document.getElementById('weight').value);
+    if (height === '' || height <= 0 || isNaN(height)) {
+        result.textContent= `Give Valid value of height`;
+    }
+    else if (weight === '' || weight <= 0 || isNaN(weight)) {
+        result.textContent= `Give Valid value of Weight`;
+    }
+    else {
+        let bmi = ((weight) / ((height / 100) ** 2));
+        result.textContent= `Result : ${bmi}`
+    }
+})
